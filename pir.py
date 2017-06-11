602f6f8dfed26a1e571e3e65e59d326daf359bab
Measure for Measure for Measure <mfmfm@Measure-for-Measure-for-Measure.local>
2017-06-11 15:59:12 +0100

    network

--- pir.py
+++ pir.py
@@ -3,17 +3,35 @@ import time, OSC, conf
 
 ON = "ON"
 OFF = "OFF"
-
 PIR_PIN = 26
+
+connected = False
+client = OSC.OSCClient()
 
 def on_transition_cb(channel):
 
+	oscmsg = OSC.OSCMessage()
+	oscmsg.setAddress(conf.MOUNT)	
+	msg = ''
+
 	if GPIO.input(channel) == 1:
 		print "transition -- ON"
+		msg = ON
 	else:
 		print "transition -- OFF"
+		msg = OFF
+
+	oscmsg.append(msg)
+	client.send(oscmsg)
 
 if __name__ == "__main__":
+
+	while not connected:
+		try:
+			client.connect((conf.HOST, conf.PORT))
+			connected = True
+		except Exception as e:
+			continue
 
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(PIR_PIN, GPIO.IN)
@@ -26,5 +44,6 @@ if __name__ == "__main__":
 	except Exception as e:
 		raise
 	finally:
+		print "clenaing up"
 		GPIO.cleanup()
 
