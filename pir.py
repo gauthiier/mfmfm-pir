d758419b6e64d8f3ac4b4c7b7cee434899ea84b7
Measure for Measure for Measure <mfmfm@Measure-for-Measure-for-Measure.local>
2017-06-11 15:41:41 +0100

    fix conflict in callbaks

--- pir.py
+++ pir.py
@@ -6,19 +6,19 @@ OFF = "OFF"
 
 PIR_PIN = 26
 
-def on_falling_transition_cb(channel):
-	print "transition -- OFF"
+def on_transition_cb(channel):
 
-def on_rising_transition_cb(channel):
-	print "transition -- ON"
+	if GPIO.input(channel) == 1:
+		print "transition -- ON"
+	else 
+		print "transition -- OFF"
 
 if __name__ == "__main__":
 
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(PIR_PIN, GPIO.IN)
 
-	GPIO.add_event_detect(PIR_PIN, GPIO.FALLING, on_falling_transition_cb, bouncetime=1000)
-	GPIO.add_event_detect(PIR_PIN, GPIO.RISING, on_rising_transition_cb, bouncetime=1000)
+	GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, on_transition_cb, bouncetime=1000)
 
 	try:
 		while True:
